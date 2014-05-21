fbcde96d12a73dc6f6d8c7a04d00ff3936385733
Moritz Ulrich <moritz@tarn-vedra.de>
2014-05-21 22:13:14 +0200

    Start passing egg-"objects" to most functions.

--- egg2nix.scm
+++ egg2nix.scm
@@ -11,24 +11,37 @@
         (create-directory dir #t))
     dir))
 
-(define (local-egg-path name)
-  (string-append (temp-directory) name "/"))
+(define (egg-name egg)
+  (if (list? egg)
+      (symbol->string (first egg))
+      (if (string? egg) egg (symbol->string egg))))
 
-(define (chicken-install-retrieve name #!optional version)
-  (process "chicken-install"
-           (list "-retrieve"
-                 (if version
-                     (string-append name ":" version)
-                     name))))
+(define (egg-version egg)
+  (if (list? egg)
+      (second egg)
+      #f))
 
-(define (nix-hash name)
-  (assert (directory? (local-egg-path name)))
-  (let-values (((in out pid) (process "nix-hash"
-                                      (list "--base32"
-                                            "--type"
-                                            "sha256"
-                                            (local-egg-path name)))))
-    (string-trim-right (read-string #f in))))
+(define (local-egg-path egg)
+  (string-append (temp-directory) (egg-name egg) "/"))
+
+(define (chicken-install-retrieve egg)
+  (let ((name (egg-name egg))
+        (version (egg-version egg)))
+    (process "chicken-install"
+             (list "-retrieve"
+                   (if version
+                       (string-append name ":" version)
+                       name)))))
+
+(define (nix-hash egg)
+  (let ((name (egg-name egg)))
+    (assert (directory? (local-egg-path egg)))
+    (let-values (((in out pid) (process "nix-hash"
+                                        (list "--base32"
+                                              "--type"
+                                              "sha256"
+                                              (local-egg-path egg)))))
+      (string-trim-right (read-string #f in)))))
 
 (define known-versions '())
 
@@ -39,85 +52,74 @@
         (begin
           (print "loading versions...")
           (with-input-from-request
-           (string-append "http://code.call-cc.org/cgi-bin/henrietta.cgi?name=" egg-name "&listversions=1")
+           (string-append "http://code.call-cc.org/cgi-bin/henrietta.cgi"
+                          "?name=" egg-name
+                          "&listversions=1")
            #f
            (lambda ()
              (let ((versions (string-split (read-string) "\n")))
                (set! known-versions (cons (cons egg-name versions) known-versions))
                versions)))))))
 
-(define (retrieve-egg name #!optional version)
+(define (retrieve-egg egg)
   (change-directory (temp-directory))
   ;; Check if `version' is a valid release.
-  (if version
-      (assert (member version (all-versions name))))
-  (let ((path (local-egg-path name)))
-    (if (not (directory? path))
-        (let*-values (((in out pid) (chicken-install-retrieve name version))
-                      ((pid normally status) (process-wait pid)))
-          (if (= 0 status)
-              path))
-        path)))
+  (let ((name (egg-name egg))
+        (version (egg-version egg)))
+    (if version
+        (assert (member version (all-versions name))))
+    (let ((path (local-egg-path egg)))
+      (if (not (directory? path))
+          (let*-values (((in out pid) (chicken-install-retrieve egg))
+                        ((pid normally status) (process-wait pid)))
+            (if (= 0 status)
+                path))
+          path))))
 
-(define (egg-meta name)
-  (car
-   (read-file (string-append (local-egg-path name)
-                             name ".meta"))))
+(define (egg-meta egg)
+  (let ((name (egg-name egg)))
+    (car
+     (read-file (string-append (local-egg-path egg)
+                               name ".meta")))))
 
-(define (egg-dependencies meta)
-  (append (cdr (or (assq 'depends meta) '(())))
-          (cdr (or (assq 'needs meta) '(())))))
+(define (egg-dependencies egg)
+  (let ((meta (egg-meta egg)))
+   (append (cdr (or (assq 'depends meta) '(())))
+           (cdr (or (assq 'needs meta) '(()))))))
 
 
-(define (egg-in-list? egg-name lst)
-  (any (lambda (x) (string= egg-name
-                            (match x
-                              ((name version)
-                               name)
-                              (name
-                               name))))
+(define (egg-in-list? egg lst)
+  (any (lambda (x) (string= (egg-name egg)
+                            (egg-name x)))
        lst))
 
-(define (egg-name egg)
-  (if (list? egg)
-      (first egg)
-      egg))
-
-(define (egg-version egg)
-  (if (list? egg)
-      (second egg)
-      #f))
-
-(define (all-dependencies-internal name #!optional version acc)
-  (let* ((name (if (symbol? name) (symbol->string name) name))
-         (version (if (number? version) (number->string version) version))
-         (name:version (if version (list name version) name)))
+(define (all-dependencies-internal egg acc)
+  (let* ((name (egg-name egg))
+         (version (egg-version egg)))
     ;; Don't collect deps if `name' is already in `acc'
-    (if (egg-in-list? name acc)
+    (if (egg-in-list? egg acc)
         acc
         (begin
-          (print (string-append "collecting deps for " name))
+          (print (string-append "collecting deps for " name " (" (or version "latest") ")"))
           ;; Make sure egg `name' is downloaded
-          (retrieve-egg name version)
-          (let ((dependencies (egg-dependencies (egg-meta name)))
+          (retrieve-egg egg)
+          (let ((dependencies (egg-dependencies egg))
                 ;; Add egg `name' to the accumulated list of deps
-                (acc (cons name:version acc)))
+                (acc (cons egg acc)))
             ;; Leaf of the dep-tree
             (if (not dependencies)
                 acc
                 ;; Add all dependencies by folding over them. This
                 ;; assures we don't load a dependency twice.
-                ;;
-                ;; NOTE: We *always* load the latest release of a
-                ;; dependency. This is BY-DESIGN.
                 (foldl (lambda (acc egg)
-                         (all-dependencies-internal (egg-name egg) #f acc))
+                         (all-dependencies-internal egg acc))
                        acc
                        dependencies)))))))
 
-(define (all-dependencies name #!optional version)
-  (remove (lambda (egg) (equal? (egg-name egg) name))
-          (all-dependencies-internal name version '())))
+(define (all-dependencies egg)
+  (let ((name (egg-name egg)))
+   (remove (lambda (egg) (equal? (egg-name egg) name))
+           (all-dependencies-internal egg '()))))
 
 (define (highest-version egg-name)
   (first (version-sort (all-versions egg-name) #f)))
@@ -134,7 +136,7 @@
          (version (or (egg-version egg)
                       (highest-version name)))
          (hash (nix-hash name))
-         (deps (all-dependencies name version)))
+         (deps (all-dependencies egg)))
     (format
      "
 ~A = eggDerivation {
@@ -160,13 +162,15 @@
 (define (nix-file spec)
   (let* ((eggs spec)
          (deps (map (lambda (egg)
-                      (all-dependencies (egg-name egg) (egg-version egg)))
+                      (all-dependencies egg))
                     eggs))
          (eggs (delete-duplicates (foldl append eggs deps)))
          (expressions (map nix-expression eggs)))
-   (with-output-to-string
-     (lambda ()
-       (print "{ stdenv, chicken, fetchegg, pkgs, eggDerivation }:")
-       (print "rec {")
-       (map print expressions)
-       (print "}\n")))))
+    (print deps)
+   ;; (with-output-to-string
+   ;;   (lambda ()
+   ;;     (print "{ stdenv, chicken, fetchegg, pkgs, eggDerivation }:")
+   ;;     (print "rec {")
+   ;;     (map print expressions)
+   ;;     (print "}\n")))
+   ))
