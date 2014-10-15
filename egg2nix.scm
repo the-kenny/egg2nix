af85cb08ebb5d819faa19b7aa662154f7e62919e
Moritz Heidkamp <moritz@twoticketsplease.de>
2014-10-15 02:24:22 +0200

    Add missing `files' unit to builtins

--- egg2nix.scm
+++ egg2nix.scm
@@ -24,6 +24,7 @@ exec csi -s "$0" "$@"
                         ports
                         lolevel
                         posix
+                        files
                         ;; Note: This is included in newer chickens?
                         ;; regex
                         srfi-1
